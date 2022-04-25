658ab63a01badc45aaa96262dc8587018b5fd5e5
Lucas Angelino <angelino.n.lucas@gmail.com>
2022-04-25 19:33:11 -0300

    RAT first commit

--- frontend/src/auth/AuthContext.jsx
+++ frontend/src/auth/AuthContext.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { ChatContext } from "../context/chat/ChatContext";
+import { AppContext } from "../context/app/AppContext";
 import { AuthRequest, Request } from "../helpers/fetch";
 import { types } from "../types/types";
 export const AuthContext = React.createContext();
@@ -15,7 +15,7 @@ const initialState = {
 
 export function AuthProvider({ children }) {
   const [auth, setAuth] = React.useState(initialState);
-  const { dispatch } = React.useContext(ChatContext);
+  const { dispatch } = React.useContext(AppContext);
 
   const login = async (email, password) => {
     const resp = await Request("login", { email, password }, "POST");
---
+++ frontend/src/components/core/App.jsx
@@ -0,0 +1,5 @@
+import * as react from "react";
+
+export function App() {
+  return <div>REPLACE THIS WITH YOU CUSTOM APP</div>;
+}
